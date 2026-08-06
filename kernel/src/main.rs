058d1ef713055765b1e382bae94179a5a6a13499
Vinz2008 <vincentbidarddelanoe@gmail.com>
2026-08-07 01:26:39 +0200

    modify comments

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -74,7 +74,7 @@ entry_point!(kernel_main);
 
 // TODO : enable stack smashing protection on the kernel (and for userspace exes ?)
 
-// TODO : add limits for processes (of memory use, etc) that can be set when doing exec (need a exec config struct)
+// TODO : add limits for processes (of memory use, of syscalls, of filesystem access (kind of like chroot ?), etc) that can be set when doing exec (need a exec config struct)
 
 // TODO : maybe in a very far future, support FRED (new technology to replace interrupts, syscalls, etc, faster, and because simpler is a little more secure) but disadvantage is as 2026, the cpus supporting have only been released a couple of months ago, and only intel cpus supports it for now
 
