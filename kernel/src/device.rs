5e72feb9bcf4b665b5a5a2b8ab724c0be89d0f12
Vinz2008 <vincentbidarddelanoe@gmail.com>
2026-08-08 17:16:06 +0200

    start working on buffering

--- kernel/src/device.rs
+++ kernel/src/device.rs
@@ -1,3 +1,4 @@
+//use arrayvec::ArrayVec;
 use lazy_static::lazy_static;
 use spin::Mutex;
 
@@ -13,9 +14,25 @@ lazy_static! {
     pub static ref STDOUT : Stdout = Stdout::new();
 }
 
+// TODO : think about the buffering strategy
+// TODO : add the buffering in the userspace, and make this not the stdout, but something like /dev/console, or /dev/console_output, then it will be assigned to the right fd, and have a stdout link/device that will select the right one depending on the process running
 
-// TODO : do buffered IO, and have stderr which would be not buffered
-struct StdoutInner;
+//const STDOUT_BUF_SIZE : usize = 4096;
+
+// TODO : have stderr which would be not buffered
+struct StdoutInner {
+ //   buf : ArrayVec<u8, STDOUT_BUF_SIZE>,
+}
+
+/*impl StdoutInner {
+    fn flush(&mut self){
+        if self.buf.is_empty(){
+            return;
+        }
+        WRITER.lock().write_bytes(&self.buf);
+        self.buf.clear();
+    }
+}*/
 
 pub struct Stdout {
     // TODO : implement a reantrant lock like in rust ?
@@ -25,9 +42,15 @@ pub struct Stdout {
 impl Stdout {
     fn new() -> Stdout {
         Stdout { 
-            stdout_inner: Mutex::new(StdoutInner),
+            stdout_inner: Mutex::new(StdoutInner {
+//                buf: ArrayVec::new(),
+            }),
         }
     }
+
+    /*pub fn flush(&self){
+        self.stdout_inner.lock().flush();
+    }*/
 }
 
 impl DeviceOps for Stdout {
@@ -37,9 +60,23 @@ impl DeviceOps for Stdout {
 
     fn write(&self, _offset : usize, data : &[u8]) -> Result<usize, FileError> {
         let stdout_lock = self.stdout_inner.lock();
-        WRITER.lock().write_bytes(data);
         
-        let _ = stdout_lock; // TODO : remove this after really using the lock for buffered io
+        // TODO : should I transform the bytes to utf str instead ?
+        /*for &byte in data {
+            if stdout_lock.buf.is_full(){
+                stdout_lock.flush();
+            }
+
+            stdout_lock.buf.push(byte);
+            if byte == b'\n' {
+                stdout_lock.flush();
+            }
+        }*/
+
+
+        WRITER.lock().write_bytes(data);
+
+        let _ = stdout_lock;
         Ok(data.len())
     }
 }
