120bf22af0d8651cc215713bfe8756a8315e59f1
Vinz2008 <vincentbidarddelanoe@gmail.com>
2026-09-24 11:25:47 +0200

    add profiling support

--- scripts/run-qemu.sh
+++ scripts/run-qemu.sh
@@ -19,7 +19,17 @@ if [ ! -f "$TEST_DISK" ]; then
     qemu-img create -f raw "$TEST_DISK" 64M
 fi
 
+
+# EXE=$(dirname $IMAGE)/rust-kernel
+# echo $EXE
+# python3 ~/osdev-tools/scripts/gen_syms.py -k $EXE -o kernel.syms
+
+# qemu flag
+# -plugin ~/osdev-tools/tools/qemu-profile-plugin/qemu-profile.so,period=10000 \
+
 exec qemu-system-x86_64 \
     -drive "format=raw,file=$IMAGE" \
     -drive format=raw,file=$TEST_DISK,id=disk2 \
     "$@"
+
+# run after python3 ~/osdev-tools/scripts/resolve_profile.py profile kernel.syms > profile-resolved.folded 
